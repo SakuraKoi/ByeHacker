8142bef5b9b2d3372a24b486d1017046130c83f8
Ldcr <ldcr993519867@gmail.com>
2018-04-17 13:05:04 +0800

    fix date

--- src/ldcr/ByeHacker/DateUtils.java
+++ src/ldcr/ByeHacker/DateUtils.java
@@ -5,11 +5,11 @@ import java.util.Date;
 
 public class DateUtils {
 	public static String formatDate(final long time) {
-		final SimpleDateFormat formater = new SimpleDateFormat("yyyy-mm-dd hh-MM-ss");
+		final SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return formater.format(time);
 	}
 	public static String formatDate(final Date time) {
-		final SimpleDateFormat formater = new SimpleDateFormat("yyyy-mm-dd hh-MM-ss");
+		final SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return formater.format(time);
 	}
 }
